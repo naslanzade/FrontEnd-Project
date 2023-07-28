8889f909409c0449bcaed3ff7d92de67a46465e9
naslanzade <novrastafa@code.edu.az>
2023-07-28 16:15:44 +0400

    add updates

--- assets/js/hit/hit.js
+++ assets/js/hit/hit.js
@@ -53,6 +53,21 @@ $(document).ready(function () {
     });
 
 
+    //Sidebar Shop
+    let filterBar = document.querySelector("#filter-for-phone .all");
+    let settingsButton = document.querySelector("#settings .all .button")
+
+    settingsButton.addEventListener("click", function () {
+        if (filterBar.classList.contains("hide-filter")) {
+            filterBar.classList.add("active-filter");
+            filterBar.classList.remove("hide-filter");
+            settingsButton.classList.add("slide-settings")
+        } else {
+            filterBar.classList.remove("active-filter");
+            filterBar.classList.add("hide-filter");
+            settingsButton.classList.remove("slide-settings")
+        }
+    })
   
   
   
